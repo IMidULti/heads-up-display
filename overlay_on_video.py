3371d27dc6e512d05737297901a271e4ede74b04
Richard Meyers <961489+rickyrem@users.noreply.github.com>
2018-07-25 18:13:37 -0700

    Use downsampling to create the datapoints for each frame

--- overlay_on_video.py
+++ overlay_on_video.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 
+import sys
 import csv
 import ffmpeg
 import argparse
 from pprint import pprint
+from pandas import read_csv
+from pandas import datetime
+from matplotlib import pyplot
 from datetime import datetime
 from datetime import timedelta
 
+pprint( sys.getrecursionlimit())
+sys.setrecursionlimit(30000)
 parser = argparse.ArgumentParser(description='script to combine video files into 1 file')
 parser.add_argument('-i', dest='input', help='video files to combine.', nargs='*')
 parser.add_argument('-o', dest='output', help='file to output the new video to.')
@@ -19,8 +25,6 @@ csv_file = args.csv
 
 ffmpeg_inputs = list()
 video_time = dict()
-
-import_image = ffmpeg.input('images/circle_with_needle.png')
 
 first_time = None
 last_time = None
@@ -41,22 +45,15 @@ for file in file_list:
     if not last_time or end_time > last_time:
         last_time = end_time
 
-#pprint(video_time)
-#pprint(first_time) 
 concated = ffmpeg.concat(*ffmpeg_inputs, v=1, a=1)
 joined = concated.node
 v = joined[0]
 #v= joined[0].overlay(import_image, x=500, y=500, enable='between(t,0,20)')
 
-data = list()
-with open(csv_file) as f:
-    reader = csv.DictReader(f)
-    data = [r for r in reader]
-
-from pandas import read_csv
-from pandas import datetime
-from matplotlib import pyplot
-
+#data = list()
+#with open(csv_file) as f:
+#    reader = csv.DictReader(f)
+#    data = [r for r in reader]
 
 def parser_func(x):
     return datetime.strptime(x, '%d-%b-%Y %H:%M:%S.%f')
@@ -65,32 +62,20 @@ series = read_csv(csv_file, header=0, parse_dates=[0], index_col=0, squeeze=True
 
 resample = series.resample('S')
 secondly_mean_values = resample.mean()
-#pprint(secondly_mean_values.to_dict())
+dataset = secondly_mean_values.to_dict()
 #secondly_mean_values.plot()
 #pyplot.show()
 
-
 processing_time = 0
-for i in range(len(data)):
-    log_entry = data[i]
-    try:
-        next_log_entry = data[i+1]
-    except IndexError:
-        continue
-    log_entry_time = datetime.strptime(log_entry['Device Time'], '%d-%b-%Y %H:%M:%S.%f')
-    next_log_entry_time = datetime.strptime(next_log_entry['Device Time'], '%d-%b-%Y %H:%M:%S.%f')
-    if log_entry_time > first_time and log_entry_time < last_time:
-        log_diff = next_log_entry_time - log_entry_time
-        processing_time += log_diff.total_seconds()
-        #pprint(processing_time)
-        #pprint(log_diff.total_seconds())
-        vehicle_speed =int(float(log_entry['Speed (OBD)(mph)']))
-        #pprint(vehicle_speed)
-        v = v.drawtext(text=vehicle_speed, fontsize=100, enable='between(t,' + str(int(float(processing_time))) + ',' + str(int(float(log_diff.total_seconds() + processing_time))) + ')')
+for time, speed in dataset['Speed (OBD)(mph)'].iteritems():
+    if time > first_time and time < last_time:
+        pprint(speed)
+        v = v.drawtext(text=speed, fontsize=100, enable='between(t,' + str(int(float(processing_time))) + ',' + str(int(float(processing_time + 1))) + ')')
+        processing_time += 1
 
 
 a = joined[1]
 out = ffmpeg.output(v, a, output_file, vcodec="h264_nvenc", preset="default",
                     pixel_format="yuva444p", video_bitrate="30M",  bufsize="30M")
 #print(' '.join(out.compile()))
-#out.run()
+out.run()
